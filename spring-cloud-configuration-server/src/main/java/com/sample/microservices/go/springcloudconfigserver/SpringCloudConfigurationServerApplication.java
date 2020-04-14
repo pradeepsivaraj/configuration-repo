16ae864a73da04dfbfd44cb9aee3045ba8588137
M1057071 <M1057071@A2ML36455.mindtree.com>
2020-04-14 22:22:11 +0530

    Newly added for config

---
+++ spring-cloud-configuration-server/src/main/java/com/sample/microservices/go/springcloudconfigserver/SpringCloudConfigurationServerApplication.java
@@ -0,0 +1,16 @@
+package com.sample.microservices.go.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigurationServerApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-configuration-server/src/test/java/com/sample/microservices/go/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.sample.microservices.go.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
